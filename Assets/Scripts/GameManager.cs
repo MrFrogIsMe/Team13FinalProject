1e278d16f6fd675cc505803b787b0c4ea88fa271
MrFrogIsMe <ribk5003@gmail.com>
2023-12-01 17:29:20 +0800

    Move TrackMouseRotation() from GameManager.Update() to Player.Update()

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,8 +17,6 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        TrackMouseRotation();
-
         if (Input.GetMouseButtonDown(0))    // left-click
         {
             if (buildingMode)
@@ -38,19 +36,6 @@ public class GameManager : MonoBehaviour
         // Respawn(10f, 10f);
     }
 
-    void TrackMouseRotation()
-    {
-        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
-
-        if (ground.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-            player.transform.LookAt(new Vector3(pointToLook.x, player.transform.position.y, pointToLook.z));
-        }
-    }
 
     // The monster will spawn randomly in the area (-_x ~ _x, -_z ~ -z)
     void Respawn(float _x, float _z)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,14 @@ public class Player : Entity
     public Dictionary<String, int> resources;
     public GameObject fireballSpawnpoint;
     public FireBall fireballTemplate;
+    Camera mainCamera;
 
     void Start()
     {
         this.Setup();
         hp = maxHp;
         healthBar.SetMaxHealth(maxHp);
+        mainCamera = FindObjectOfType<Camera>();
         resources = new Dictionary<String, int>();
         resources.Add("Tree", 0);
         resources.Add("Stone", 0);
@@ -21,6 +23,7 @@ public class Player : Entity
     void Update()
     {
         this.Move();
+        this.TrackMouseRotation();
 
         if (hp <= 0)
         {
@@ -51,5 +54,19 @@ public class Player : Entity
         fireballTemplate.transform.forward = this.transform.forward;
         fireballTemplate.damage = this.damage;
         Instantiate(fireballTemplate);
+    }
+
+    void TrackMouseRotation()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            this.transform.LookAt(new Vector3(pointToLook.x, this.transform.position.y, pointToLook.z));
+        }
     }
 }
