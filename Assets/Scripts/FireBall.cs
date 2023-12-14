e6b4999727d6afc186ae0cf21062145622a03e70
lullabynagi <74042520+lullabynagi@users.noreply.github.com>
2023-12-14 15:16:53 +0800

    test_10

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -5,27 +5,38 @@ public class FireBall : MonoBehaviour
     public int damage;
     float speed = 50f;
     Vector3 movement;
+
+
+
+    public GameObject ExplosionPrefab;
     Rigidbody rb;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         Destroy(this.gameObject, 2f);
+
     }
 
     void Update()
     {
+        
         rb.velocity = transform.forward.normalized * speed;
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
         Destroy(this.gameObject);
+        Instantiate(ExplosionPrefab, this.transform.position, this.transform.rotation);
+
         // print($"hit: {other.tag}");
         if (other.CompareTag("Monster"))
+
         {
             // print($"{other.gameObject}");
             other.GetComponent<Monster>().TakeDamage(damage);
+
             // print($"{other.name}.hp = {other.GetComponent<Entity>().hp}");
         }
     }
