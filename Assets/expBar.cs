922a28447a20abde28f54b990b99a32dbcf1858c
Worldsixthocean <oceanwong524@gmail.com>
2023-12-18 00:48:31 +0800

    expBar animation

--- Assets/expBar.cs
+++ Assets/expBar.cs
@@ -10,18 +10,42 @@ public class expBar : MonoBehaviour
     [SerializeField] float exp;
     [SerializeField] int level;
 
-    [SerializeField] Slider slider;
+    [SerializeField] Slider slider1;
     [SerializeField] text text;
+    [SerializeField] RectTransform slider2;
+
+    [Header("AnimationUtil")]
+    [SerializeField] private AnimationCurve _curve;
+    [SerializeField] private float from;
+    [SerializeField] private float to;
+    [SerializeField] private float fade;
+    [SerializeField] private float fadeDuration;
 
     public void setExp(int expArg) {
+        from = exp;
+        fade = 0;
+
         exp = expArg;
-        slider.value = exp > maxExp ? maxExp : exp ;
+        slider2.GetComponent<Image>().fillAmount = exp / maxExp;
+        to = exp;
     }
     public void setLevel(int levelArg, int maxExpArg) { 
         level = levelArg;
         maxExp = maxExpArg;
-        slider.maxValue = maxExp;
-        slider.value = 0;
+        
+        slider1.maxValue = maxExp;
         text.text = levelArg.ToString();
+    }
+
+    private void Update()
+    {
+        if (fade + Time.deltaTime >= fadeDuration) {
+            fade = fadeDuration;
+        }
+        else{
+            fade += Time.deltaTime;
+        }
+
+        slider1.value = Mathf.Lerp(from, to, _curve.Evaluate(fade / fadeDuration));
     }
 }
