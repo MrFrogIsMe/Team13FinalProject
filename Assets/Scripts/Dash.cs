6e365543183547fd99e4c57ded6150eca7fe2b05
MrFrogIsMe <ribk5003@gmail.com>
2024-01-05 03:37:55 +0800

    Added Dash ability Description:     Press Shift to dash

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+class Dash : Ability
+{
+    public float dashSpeed;
+    float originalMaxSpeed;
+
+    public override void Activate()
+    {
+        originalMaxSpeed = player.maxSpeed;
+        player.maxSpeed = dashSpeed;
+        Invoke(nameof(DelayedApplyForce), 0.025f);
+    }
+
+    public override void Deactivate()
+    {
+        player.maxSpeed = originalMaxSpeed;
+    }
+
+    void DelayedApplyForce()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(direction.normalized * dashSpeed, ForceMode.Impulse);
+    }
+}
